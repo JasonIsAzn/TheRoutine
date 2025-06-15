d1cf77715d513e8cbf791f17a3c7a18fab17b275
Jason <lejason123@gmail.com>
2025-06-15 10:36:25 -0500

    migrate to expo router and yarn

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-center text-base text-gray-800">
+                Edit <Text className="font-semibold">app/index.tsx</Text> to edit this screen.
+            </Text>
+        </View>
+    );
+}
