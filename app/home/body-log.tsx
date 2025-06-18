2c00baadb7e3b23dcfbc9fa76d478b57c4eb9560
Jason <lejason123@gmail.com>
2025-06-18 11:31:07 -0500

    home routing

---
+++ app/home/body-log.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function BodyLogScreen() {
+    return (
+        <View className="flex-1 justify-center items-center bg-white">
+            <Text className="text-xl">📷 thiccness log</Text>
+        </View>
+    );
+}
