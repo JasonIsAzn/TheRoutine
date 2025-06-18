2bc62c798d0dd0ee53d2f2e6a00f687bb94e5021
Jason <lejason123@gmail.com>
2025-06-17 19:50:43 -0500

    fix database connection

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5284/api',
+    baseURL: 'http://192.168.1.99:5284/api', // Replace with your API base URL
     headers: {
         'Content-Type': 'application/json',
     },
