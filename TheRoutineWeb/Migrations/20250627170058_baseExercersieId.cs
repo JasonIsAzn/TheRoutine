03d1c6482da12717e981a11aa7426d932afbc9e7
Jason <lejason123@gmail.com>
2025-06-27 12:01:52 -0500

    add base exercise id to workout session exercise

---
+++ TheRoutineWeb/Migrations/20250627170058_baseExercersieId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheRoutineWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class baseExercersieId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BaseExerciseId",
+                table: "WorkoutSessionExercises",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkoutSessionExercises_BaseExerciseId",
+                table: "WorkoutSessionExercises",
+                column: "BaseExerciseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutSessionExercises_BaseExercises_BaseExerciseId",
+                table: "WorkoutSessionExercises",
+                column: "BaseExerciseId",
+                principalTable: "BaseExercises",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutSessionExercises_BaseExercises_BaseExerciseId",
+                table: "WorkoutSessionExercises");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkoutSessionExercises_BaseExerciseId",
+                table: "WorkoutSessionExercises");
+
+            migrationBuilder.DropColumn(
+                name: "BaseExerciseId",
+                table: "WorkoutSessionExercises");
+        }
+    }
+}
--- TheRoutineWeb/Models/WorkoutSessionExercise.cs
+++ TheRoutineWeb/Models/WorkoutSessionExercise.cs
@@ -12,7 +12,20 @@ namespace TheRoutineWeb.Models
         public bool IsCompleted { get; set; } = false;
         public bool IsSkipped { get; set; } = false;
         public bool IsDeleted { get; set; } = false;
+        public int? BaseExerciseId { get; set; }
+        public BaseExercise? BaseExercise { get; set; }
 
         public WorkoutSession WorkoutSession { get; set; } = default!;
+
+        public static WorkoutSessionExercise FromWorkoutExercise(WorkoutExercise ex)
+        {
+            return new WorkoutSessionExercise
+            {
+                Name = ex.Name,
+                Muscles = new List<string>(ex.Muscles),
+                Order = ex.Order,
+                IsOptional = ex.IsOptional
+            };
+        }
     }
 }
