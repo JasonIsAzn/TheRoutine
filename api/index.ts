e5e254a7a153beb196bacd0b84ba991053332a91
Jason <lejason123@gmail.com>
2025-06-30 11:27:15 -0500

    add local ip for base url for ease of development

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://theroutine-dccgcpdrckgqhahh.canadacentral-01.azurewebsites.net/api',
+    // baseURL: 'http://172.20.10.3:5284/api',
     headers: {
         'Content-Type': 'application/json',
     },
