459c68f0aaaa5d379ca08201a291050070c6581d
Jason <lejason123@gmail.com>
2025-07-16 22:41:47 -0500

    improve workout plan info design

--- app/home/workout-plan/workout-plan-info.tsx
+++ app/home/workout-plan/workout-plan-info.tsx
@@ -3,6 +3,7 @@ import { useWorkoutPlan } from '../../../hooks/useWorkoutPlan';
 import { useRouter, useFocusEffect } from 'expo-router';
 import { useCallback, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { WorkoutDay } from 'types/workout';
 
 
 const fullDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -18,7 +19,18 @@ export default function WorkoutPlanInfoScreen() {
     );
 
     useEffect(() => {
-        setExpandedDays(new Set());
+        if (plan) {
+            const todayIndex = new Date().getDay();
+            const expanded = new Set<number>();
+
+            const today = plan.workoutDays.find((day: WorkoutDay) => day.order === todayIndex);
+            if (today && today.exercises.length > 0) {
+                const index = plan.workoutDays.findIndex((day: WorkoutDay) => day.order === todayIndex);
+                if (index !== -1) expanded.add(index);
+            }
+
+            setExpandedDays(expanded);
+        }
     }, [plan]);
 
     const toggleExpandDay = (index: number) => {
@@ -43,49 +55,49 @@ export default function WorkoutPlanInfoScreen() {
 
     return (
         <ScrollView className="flex-1 bg-background p-4">
-            <Text className="text-2xl font-bold mb-1">{plan.name}</Text>
-            <Text className="text-xs text-gray-500 mb-3">
-                Version: {plan.planGroupId}.{plan.version}
-            </Text>
-            <Text className="text-sm text-gray-600 mb-4">
-                Split Type: {plan.splitType} | Cycle Length: {plan.cycleLength} days
-            </Text>
+            <Text className="text-2xl font-bold mb-12">{plan.name}</Text>
 
             {plan.workoutDays.map((day: any, i: number) => (
                 <View key={day.order} className="mb-5">
-                    <View className="flex-row items-center w-full mb-5">
-                        {/* Chevron – 15% width */}
-                        {day.exercises.length !== 0 ? (
-                            <Pressable onPress={() => toggleExpandDay(i)} className="w-[9%] py-1 items-startjustify-center">
-                                <FontAwesomeIcon
-                                    icon={['fas', expandedDays.has(i) ? 'chevron-up' : 'chevron-down']}
-                                    size={18}
-                                    color="#000"
-                                />
-                            </Pressable>
+                    <View className="w-full mb-5 flex-row items-center">
+                        {day.exercises.length > 0 ? (
+                            <>
+                                {/* Chevron – 10% width */}
+                                <Pressable onPress={() => toggleExpandDay(i)} className="w-[10%] items-start justify-center py-1">
+                                    <FontAwesomeIcon
+                                        icon={['fas', expandedDays.has(i) ? 'chevron-up' : 'chevron-down']}
+                                        size={18}
+                                        color="#000"
+                                    />
+                                </Pressable>
+
+                                {/* Day Label – flex-1 */}
+                                <View className="flex-1 pr-2">
+                                    <Text
+                                        className="font-bold text-[18px] leading-[22px]"
+                                        numberOfLines={1}
+                                        ellipsizeMode="tail"
+                                    >
+                                        {day.label}
+                                    </Text>
+                                </View>
+
+                                {/* Day Name – 20% width */}
+                                <View className="w-[20%] items-end">
+                                    <Text className="text-gray font-semibold text-sm">
+                                        {fullDayNames[day.order]}
+                                    </Text>
+                                </View>
+                            </>
                         ) : (
-                            <View className="w-[9%]"></View>
+                            // Rest Day layout (centered)
+                            <View className="flex-1 items-center">
+                                <Text className="text-sm font-semibold text-gray">
+                                    {day.label} – {fullDayNames[day.order]}
+                                </Text>
+                            </View>
                         )}
-
-                        {/* Day Label – 65% width */}
-                        <View className="flex-1 pr-2">
-                            <Text
-                                className="font-bold text-[18px] leading-[22px]"
-                                numberOfLines={1}
-                                ellipsizeMode="tail"
-                            >
-                                {day.label}
-                            </Text>
-                        </View>
-
-                        {/* Day Name – 20% width */}
-                        <View className="w-[20%] items-end">
-                            <Text className="text-gray font-semibold text-sm">
-                                {fullDayNames[day.order]}
-                            </Text>
-                        </View>
                     </View>
-
 
                     {expandedDays.has(i) && (
                         <>
