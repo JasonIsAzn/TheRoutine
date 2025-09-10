bd6420695324a3239bedd6cc793a956a0e9af114
Jason <lejason123@gmail.com>
2025-09-10 17:05:41 -0500

    update api endpoint

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://theroutine-dccgcpdrckgqhahh.canadacentral-01.azurewebsites.net/api',
-    // baseURL: 'http://172.20.10.3:5284/api',
+    baseURL: 'https://theroutine-production.up.railway.app/api',
+    // baseURL: 'http://192.168.1.11:5284/api',
     headers: {
         'Content-Type': 'application/json',
     },
